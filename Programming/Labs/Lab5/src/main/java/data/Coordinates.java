9f438d4973454a4d7b0414a7f091973faa2137cd
Ariunbat Tserendorj <tserendorjariunbat@gmail.com>
2026-02-14 19:13:56 +0300

    push

--- Programming/Labs/Lab5/src/main/java/data/Coordinates.java
+++ Programming/Labs/Lab5/src/main/java/data/Coordinates.java
@@ -1,4 +1,6 @@
 package data;
 
 public class Coordinates {
+    private double x; //Значение поля должно быть больше -431
+    private Integer y; //Поле не может быть null
 }
--- Programming/Labs/Lab5/src/main/java/data/Studio.java
+++ Programming/Labs/Lab5/src/main/java/data/Studio.java
@@ -1,4 +1,5 @@
 package data;
 
 public class Studio {
+    private String name; //Поле может быть null
 }
